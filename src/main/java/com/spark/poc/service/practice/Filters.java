db2ace1a70c04b4ba81f3961802c0083d308da62
Dharmendra Kumar <dhirubhaigpl@gmail.com>
2024-08-12 15:34:17 +0530

    flatmaping and filters

--- src/main/java/com/spark/poc/service/practice/Filters.java
+++ src/main/java/com/spark/poc/service/practice/Filters.java
@@ -8,13 +8,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class FlatMapping {
+public class Filters {
 
-    public static void main(String[] arg){
+    public static void main(String[] args) {
         JavaSparkContext jsc = Utils.getSparkConfig();
-        flatMappingExample(jsc);
+        filterExample(jsc);
     }
-    private static void flatMappingExample(JavaSparkContext jsc){
+    private static void filterExample(JavaSparkContext jsc){
         List<String> inputData = new ArrayList<>();
         inputData.add( "WARN: Tuesday 4 September 0405");
         inputData.add("WARN: Tuesday 5 September 0408");
@@ -22,8 +22,9 @@ public class FlatMapping {
         inputData.add("WARN: Tuesday 6 September 0430");
         inputData.add("FATAL: Tuesday 7 September 0429");
 
-        JavaRDD<String> javaRDD= jsc.parallelize(inputData);
-        JavaRDD<String> words = javaRDD.flatMap(value -> Arrays.asList(value.split(" ")).iterator());
-        words.foreach(value -> System.out.println(value));
+        jsc.parallelize(inputData)
+                .flatMap(value -> Arrays.asList(value.split(" ")).iterator())
+                .filter(word -> word.length()>1)
+                .foreach(value -> System.out.println(value));
     }
 }
--- src/main/java/com/spark/poc/service/practice/RDDExample.java
+++ src/main/java/com/spark/poc/service/practice/RDDExample.java
@@ -1,15 +1,13 @@
 package com.spark.poc.service.practice;
 
+import com.google.common.collect.Iterables;
 import com.spark.poc.service.config.Utils;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
